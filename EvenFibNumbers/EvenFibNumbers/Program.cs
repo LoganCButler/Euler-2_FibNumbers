73a284be5e1c997b922f31dea5d5fe77425e412a
Logan Butler <logan.butler@agilethought.com>
2016-07-21 14:27:13 -0400

    initial commit

---
+++ EvenFibNumbers/EvenFibNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logic;
+
+namespace EvenFibNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ EvenFibNumbers/Logic/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class AppLogic
+    {
+        public int answer;
+
+        int topNumber = 4000000;
+        bool underTopNumber = true;
+        public int getEvenFibNumbers()
+        {
+            int a = 1;
+            int b = 2;
+            int sum = 0;
+            while (underTopNumber)
+            {
+                if (b%2 == 0)
+                {
+                    sum += b;
+                }
+            }
+      
+            return answer;
+        }
+    }
+}
